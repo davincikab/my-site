ea3d770273ac0abfbada0efe4afa4e1be20d6d51
davincikab <davidnganganjeri079@gmail.com>
2020-06-23 08:34:40 +0300

    updated the ui with actual project images and description

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ window.onscroll = function(e) {
 var scroll = window.requestAnimationFrame ||
             function(callback){ window.setTimeout(callback, 1000/60)};
 
-var scrollElements = document.querySelectorAll('.media');
+var scrollElements = null;
 
 function loop(){
     scrollElements.forEach(el => {
@@ -43,7 +43,7 @@ function loop(){
     scroll(loop);
 }
 
-loop();
+// loop();
 
 // determine if the element is within viewport
 function isElementInViewPort(el){
@@ -81,13 +81,56 @@ function isElementInViewPort(el){
 // });
 
 // load the data
-// fetch(dataUrl)
-//     .then(res => res.json())
-//     .then(data => {
-//         updateProject(data);
-//     });
+fetch(dataUrl)
+    .then(res => res.json())
+    .then(data => {
+        updateProject(data);
+    });
 
 function updateProject(data){
+    var keys = Object.keys(data);
+    console.log(keys);
+
+    keys.forEach(key => {
+        createMediaCard(data[key], key);
+          
+    });
+
+    // call loop
+    setTimeout(function(e){
+       scrollElements =  document.querySelectorAll('.media');
+       loop();
+    }, 1000)
+    
+}
+
+function createMediaCard(projects, key) {
+    let element = document.getElementById(key);
+    let docFragment = document.createDocumentFragment();
+    let mediaElements = [];
+    projects.forEach(project => {
+        let imageUrl = key + '/' +project.image_url + '/'+project.images[0];
+        let mediaContainer = document.createElement('div');
+        mediaContainer.setAttribute('class','media');
+
+        // create a media section
+         mediaContainer.innerHTML = `<img src="./images/${imageUrl}" alt="" class="media-image">
+                        <h4 class="media-title text-center bg-primary">${project.name}</h4>
+                        <div class="media-description text-center">
+                            ${project.description}
+                        </div>`;
+
+        // append to docFragment
+        docFragment.appendChild(mediaContainer);
+
+      
+    });
+
+    console.log(docFragment);
+    // append to the html
+    // element.innerHTML = mediaElements.toString();
+    element.innerHTML = '';
+    element.appendChild(docFragment);
 
 }
 
